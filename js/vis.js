f568fe96db11b167517ef8cee53478af94b1e67a
Adeleke Olasope <36890804+unpoint@users.noreply.github.com>
2022-06-28 16:39:09 +0100

    updating visualiser and editing homepage

--- js/vis.js
+++ js/vis.js
@@ -1,25 +1,28 @@
 let theImage = document.querySelector('#theImage');
 let scene = "";
 let onWallTile = document.querySelector('.imageContainer');
+let home = document.querySelector('.home');
+// let return = document.querySelector('#return');
 
 const changeScene = (sceneArg) => {
     if(sceneArg == 'onwall'){
-        theImage.src = sceneArg + "/" + sceneArg + "_default.png";
+        theImage.src = `images/${sceneArg}/${sceneArg}_default.png`;
     }
     else if(sceneArg != '') {
-        theImage.src = sceneArg + "/" + sceneArg + "_default.jpg";
+        theImage.src = `images/${sceneArg}/${sceneArg}_default.jpg`;
     }
     console.log(theImage.src);
     scene = sceneArg;
+    home.style.display = "none";
 }
 const changeImage = (imageName) => {
     if(scene == 'onwall'){
         onWallTile.style.backgroundImage = "url('/Ceras/onwall/" + imageName + ".jpg')"
     }
     else if(scene != ""){
-        theImage.src = scene + "/" + scene + "_" + imageName + ".jpg";
+        theImage.src = `${scene}/${scene}_${imageName}.jpg`;
     }
     else {
-        theImage.src = "#"
+        theImage.src = "#";
     }
 }
